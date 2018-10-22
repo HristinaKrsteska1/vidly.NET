99b7e21a778b837cd603308d6bab5479df5e1097
HristinaKrsteska1 <hristina.krsteska1@gmail.com>
2018-10-22 19:03:24 +0200

    Add movie model and controller

---
+++ vidly.NET/Controllers/MoviesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using vidly.NET.Models;
+
+namespace vidly.NET.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie()
+            {
+                Name = "Die Hard"
+            };
+            return View(movie);
+        }
+    }
+}
