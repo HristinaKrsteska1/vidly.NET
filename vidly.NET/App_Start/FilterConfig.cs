6a1dd87a6d068a36ab6bc67adcf0e8b510162222
HristinaKrsteska1 <hristina.krsteska1@gmail.com>
2018-10-30 15:01:09 +0100

    Prevent anonymous access globally.

--- vidly.NET/App_Start/FilterConfig.cs
+++ vidly.NET/App_Start/FilterConfig.cs
@@ -8,7 +8,7 @@ namespace vidly.NET
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
-            
+            filters.Add(new AuthorizeAttribute());
         }
     }
 }
--- vidly.NET/Controllers/HomeController.cs
+++ vidly.NET/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace vidly.NET.Controllers
 {
-    
+    [AllowAnonymous]
     public class HomeController : Controller
     {
         public ActionResult Index()
